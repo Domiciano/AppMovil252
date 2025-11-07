005be1e406c5be6be5c5ad8c10f518f586ec82a9
Domiciano Rincón <domiciano.rincon@correounivalle.edu.co>
2025-11-07 12:45:03 -0500

    Semana 16

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:moviles252/features/auth/ui/bloc/login_bloc.dart';
 import 'package:moviles252/features/auth/ui/bloc/signup_bloc.dart';
 import 'package:moviles252/features/profiles/ui/bloc/profiles_list_bloc.dart';
@@ -13,19 +16,58 @@ import 'package:moviles252/features/chat/ui/bloc/chat_bloc.dart';
 import 'package:moviles252/features/chat/ui/screens/chat_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('🔙 Mensaje recibido en background: ${message.messageId}');
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  // Manejo de notificaciones en background
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // Configura canal local
+  const AndroidInitializationSettings initSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+  const InitializationSettings initSettings =
+      InitializationSettings(android: initSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initSettings);
 
   await Supabase.initialize(
     url: 'https://yzosfzyewkdpnmlbgbej.supabase.co',
     anonKey: 'sb_publishable_sVlCTCKFQ9NktJjQTOmahw_QoBUGODX',
   );
 
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+class App extends StatefulWidget{
+  const App({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+  
+}
+
+
+class MyAppState extends State<App> {
+
+  int id = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _initNotifications();
+  }
 
   // This widget is the root of your application.
   @override
@@ -56,4 +98,51 @@ class MyApp extends StatelessWidget {
       },
     );
   }
+
+
+  Future<void> _initNotifications() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    // 🔐 Solicita permisos (solo necesario en iOS)
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    print('Permisos otorgados: ${settings.authorizationStatus}');
+
+    // 🔔 Suscripción al topic
+    await messaging.subscribeToTopic("mi_topic_general");
+    print("✅ Suscrito al topic mi_topic_general");
+
+    // 📩 Cuando la app está en foreground
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print("💬 Mensaje recibido en foreground: ${message.data}");
+
+      RemoteNotification? notification = message.notification;
+      AndroidNotification? android = message.notification?.android;
+      
+        flutterLocalNotificationsPlugin.show(
+          id++,
+          "Nuevo mensaje",
+          "${message.data}",
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              'canal_notif', // ID único del canal
+              'Notificaciones generales',
+              importance: Importance.max,
+              priority: Priority.high,
+            ),
+          ),
+        );
+      
+    });
+
+    // 🔙 Cuando la app fue abierta desde una notificación
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('🔓 Notificación abrió la app: ${message.data}');
+    });
+  }
+
 }
